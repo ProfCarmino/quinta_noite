2e0c92b685e408fab4be089d6759b2ee12666fd6
ProfCarmino <73600424+ProfCarmino@users.noreply.github.com>
2021-09-30 18:54:14 -0300

    Final Aula Keyt

--- mongodb/src/app/app.module.ts
+++ mongodb/src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+
 import { AppComponent } from './app.component';
 import { ClienteinserirComponent } from './clientes/cliente-inserir/cliente-inserir.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 
 @NgModule({
   declarations: [
-    AppComponent, ClienteinserirComponent
+    AppComponent, ClienteinserirComponent, CabecalhoComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, BrowserAnimationsModule, MatInputModule, MatButtonModule, MatCardModule, MatToolbarModule, MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
